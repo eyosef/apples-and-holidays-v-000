977cb6ad545668934748e7f20fe445b25db0dd04
Eden Mesfun <emesfun@gmail.com>
2018-01-30 15:23:43 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ end
 def all_supplies_in_holidays(holiday_hash)
   # holiday_hash.fetch(:winter)
   formatted_hash = holiday_hash.to_a
-  formatted_hash.each do { |seasons, holidays| seasons.capitalize && holidays.capitalize}
+  formatted_hash.each { |seasons, holidays| seasons.capitalize && holidays.capitalize}
     #formatted_holiday_list.merge!({seasons.capitalize => holidays.capitalize})
   end #each
   formatted_hash
