dbfc50a3c701e57e553404d39f7deff52b0e4bf2
Eden Mesfun <emesfun@gmail.com>
2018-01-30 15:39:42 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
   formatted_hash = holiday_hash.to_a
-  holiday_hash.each do |season|
+  holiday_hash.each do |season, holiday|
     season.uppercase
   end
 
