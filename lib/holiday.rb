9a1bf8c66fac0c5f05209c0bdf5800884cd6ce40
Eden Mesfun <emesfun@gmail.com>
2018-01-30 16:26:08 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
   formatted.each do |season, holidays|
     puts "#{season.capitalize}:"
   formatted.each do |holidays, supplies|
-    puts "#{holidays.capitalise}"
+    puts "#{holidays.capitalize}"
   end #each
   #hash.index(value)
   # iterate through holiday_hash and print items such that your readout resembles:
