5cda1e9b20970de2ca797284f04f33534ba01fcd
Eden Mesfun <emesfun@gmail.com>
2018-01-30 16:34:05 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
 
   formatted = holiday_supplies.to_a
   formatted.each do |season, holidays|
-    puts "#{season.capitalize!}:"
+    puts "#{season.capitalize}:"
   end #each
   formatted
 
