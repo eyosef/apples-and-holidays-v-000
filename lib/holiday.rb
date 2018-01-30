d0eb1334238efc4ddf8571c1bcabd5f8287fdadf
Eden Mesfun <emesfun@gmail.com>
2018-01-30 17:17:11 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,6 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_supplies.each do |season, holidays|
     puts "#{season.capitalize}:"
   holidays.each do |holidays, supplies|
-    holidays.delete("_")
     puts "  #{holidays.capitalize}: #{supplies.join(", ")}"
   end #each
 end #each
