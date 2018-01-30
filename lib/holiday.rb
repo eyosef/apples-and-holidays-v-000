1c22177ce32b7d5e4335659538e98274d44e3c19
Eden Mesfun <emesfun@gmail.com>
2018-01-30 16:20:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
 
   formatted = holiday_supplies.to_a
   formatted.each do |season, holiday|
-    if season == :winter
+    if season == :winter || :spring
       :winter.to_s
     elsif season == :spring
       :spring.to_s
